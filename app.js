76a652c1a85f7e1ae4f1916fe5d81b92ef7a2296
Zameel <zekyazelu7@gmail.com>
2020-06-23 12:40:10 +0530

    Auto pick background color added

--- app.js
+++ app.js
@@ -13,3 +13,55 @@ addEventListener('load', () => {
 
   callApi();
 });
+function setbackground()
+{
+    window.setTimeout( "setbackground()", 5000); //  milliseconds delay
+
+    var index = Math.round(Math.random() * 9);
+
+    var ColorValue = "FFFFFF"; // default color - white (index = 0)
+
+    if(index == 1)
+        ColorValue = "66FF33"; 
+    if(index == 2)
+        ColorValue = "FF0000"; 
+    if(index == 3)
+        ColorValue = "FF00FF"; 
+    if(index == 4)
+        ColorValue = "0000FF"; 
+    if(index == 5)
+        ColorValue = "00FFFF"; 
+    if(index == 6)
+        ColorValue = "FFFF00"; 
+    if(index == 7)
+       ColorValue = "CC66FF"; 
+    if(index == 8)
+        ColorValue = "3366FF"; 
+   if(index == 9)
+        ColorValue = "CCCCCC"; 
+
+   document.getElementsByTagName("body")[0].style.backgroundColor = "#" + ColorValue;
+
+}
+
+function setbackgroundTitle()
+{
+    window.setTimeout( "setbackgroundTitle()", 5000); //  milliseconds delay
+
+    var index = Math.round(Math.random() * 4);
+
+    var ColorValue = "FFFFFF"; // default color - white (index = 0)
+
+    if(index == 1)
+        ColorValue = "66FF33"; 
+    if(index == 2)
+        ColorValue = "FF0000"; 
+    if(index == 3)
+        ColorValue = "FF00FF"; 
+    if(index == 4)
+        ColorValue = "0000FF"; 
+
+
+    document.getElementById("title").style.backgroundColor = "#" + ColorValue;
+
+}
